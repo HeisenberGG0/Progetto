f970877e491499d993243fc741756eda643a55cd
Manu2791 <96333747+Manu2791@users.noreply.github.com>
2022-01-01 16:16:14 +0100

    Update Sala.java

--- Ristoranti/src/Sala.java
+++ Ristoranti/src/Sala.java
@@ -1,4 +1,53 @@
 
-public class Sala {
 
+  public class Sala {
+
+	private String NomeSala;
+	private int Capienza;
+	private enum TipodiSala {INGRESSO, ADULTI, RAGAZZI, BAMBINI};
+	private boolean AngoloRistoro;
+	private enum Design {CLASSICO, RUSTICO, MODERNO, ELEGANTE, ANTICO};
+	
+	private Ristorante AppartienealRistorante;
+
+	
+	public Sala(String NomeSala) {
+		this.setNomeSala(NomeSala);
+	
+
+		
+	}
+	
+	
+	public void stampa(){
+		System.out.println("Sala "+this.NomeSala);
+	}
+	
+	
+	public Ristorante getAppartienealRistorante() {
+		return AppartienealRistorante;
+	}
+
+	public void setAppartienealRistorante(Ristorante AppartienealRistorante) {
+		if(AppartienealRistorante==null){
+			throw new IllegalArgumentException("La Sala non appartiene a nessun ristorante");
+		}
+		this.setAppartienealRistorante(AppartienealRistorante);
+	}
+
+
+
+	public String getNomeSala() {
+		return NomeSala;
+	}
+
+
+
+	public void setNomeSala(String nomeSala) {
+		NomeSala = nomeSala;
+	}
 }
+
+  
+
+
